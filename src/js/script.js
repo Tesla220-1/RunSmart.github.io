3051e41f6b4f9aaa6ff76126f691103c3bebf79c
Vlad <vladislav5008zo@gmail.com>
2020-01-24 15:09:01 +0300

    slider wit ttiny slider plugin ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.carousel__inner').slick({
       infinite: true,
       speed: 1000,
@@ -18,4 +18,22 @@ $(document).ready(function(){
       ]
     });
   });
-      
+       */
+
+
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false, 
+  nav: false
+});
+
+document.querySelector('.prev').addEventListener('click', function(){
+  slider.goTo('prev')
+});
+
+document.querySelector('.next').addEventListener('click', function(){
+  slider.goTo('next')
+});
